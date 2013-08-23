fec4b8ed974da4d39f98ff7b15f81adf0188974c
Jason Hooper <jason.hooper@talk21.com>
2013-08-23 15:59:42 +0100

    after my pages and sections

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,61 @@
 class PagesController < ApplicationController
+
+  def index
+    list
+	render('list')
+  end
+  
+  def list
+    @pages = Page.order("pages.position ASC")
+  end
+  
+  def show
+    @page = Page.find(params[:id])
+  end
+  
+  def new
+    @page = Page.new
+  end
+  
+  def create
+    # instantiate a new object using form parameters
+	@page = Page.new(params[:page])
+	# save the object
+	if @page.save
+	  # if save succeeds, redirect to the list action
+	  flash[:notice] = "Page created."
+	  redirect_to(:action => 'list')
+	else
+	  # if save fails, redisplay the form so user can fix problems
+	  render('new')
+	end
+  end
+  
+  def edit
+    @page = Page.find(params[:id])
+  end
+  
+  def update
+    # find object using form parameters
+	@page = Page.find(params[:id])
+	# update the object
+	if @page.update_attributes(params[:page])
+	  # if update succeeds, redirect to the list action
+	  flash[:notice] = "Page updated."
+	  redirect_to(:action => 'show', :id => @page.id)
+	else
+	  # if save fails, redisplay the form so user can fix problems
+	  render('edit')
+	end
+  end
+  
+  def delete
+    @page = Page.find(params[:id])
+  end
+  
+  def destroy
+    Page.find(params[:id]).destroy
+	flash[:notice] = "Page destroyed."
+	redirect_to(:action => 'list')
+  end
 end
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,2 +1,62 @@
 class SectionsController < ApplicationController
+
+  def index
+    list
+	render('list')
+  end
+  
+  def list
+    @sections = Section.order("sections.position ASC")
+  end
+  
+  def show
+    @section = Section.find(params[:id])
+  end
+  
+  def new
+    @section = Section.new
+  end
+  
+  def create
+    # instantiate a new object using form parameters
+	@section = Section.new(params[:section])
+	# save the object
+	if @section.save
+	  # if save succeeds, redirect to the list action
+	  flash[:notice] = "Section created."
+	  redirect_to(:action => 'list')
+	else
+	  # if save fails, redisplay the form so user can fix problems
+	  render('new')
+	end
+  end
+  
+  def edit
+    @section = Section.find(params[:id])
+  end
+  
+  def update
+    # find object using form parameters
+	@section = Section.find(params[:id])
+	# update the object
+	if @section.update_attributes(params[:section])
+	  # if update succeeds, redirect to the list action
+	  flash[:notice] = "Section updated."
+	  redirect_to(:action => 'show', :id => @section.id)
+	else
+	  # if save fails, redisplay the form so user can fix problems
+	  render('edit')
+	end
+  end
+  
+  def delete
+    @section = Section.find(params[:id])
+  end
+  
+  def destroy
+    Section.find(params[:id]).destroy
+	flash[:notice] = "Section destroyed."
+	redirect_to(:action => 'list')
+  end
+
 end
